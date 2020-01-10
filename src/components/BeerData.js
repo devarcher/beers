72b9d6569f06fe92f698dc71d49d499bc1ef2b4e
devarcher <archershot@hotmail.com>
2020-01-09 22:45:17 -0600

    fetch to state complete

--- src/components/BeerData.js
+++ src/components/BeerData.js
@@ -3,24 +3,46 @@ import React, { Component } from 'react';
 class BeerData extends Component {
   state = {
     isLoading: true,
+    beer: null
   }
   
   componentDidMount() {
     this.fetchBeers();
   }
   
-  fetchBeers = () => {
-    fetch("https://api.punkapi.com/v2/beers")
-    .then(res => res.json())
-    .then(data => console.log(data));
+  async fetchBeers() {
+    const url = "https://api.punkapi.com/v2/beers";
+    const response = await fetch(url);
+    const data = await response.json();
+    const filteredBeerData = data.map(beer => (
+      {
+        id: beer.id,
+        name: beer.name,
+        tagline: beer.tagline,
+        description: beer.description,
+        abv: beer.abv,
+        ibu: beer.ibu
+      }
+    ))
+    this.setState({ beer: filteredBeerData, isLoading: false })
+    console.log('***STATE BEER***' , this.state.beer)
   }
+
+  
+
 
   render() {
     return (
     <div>
-      {this.state.loading ? <div>loading ... </div> : <div>beers</div>}
+      {this.state.loading || !this.state.beer ? ( 
+        <div>loading ... </div>
+      ) : ( 
+      <div>
+        <div>{this.state.beer[0].name}</div>
+      </div>
+      )}
     </div>
-    )
+    );
   }
 }
 
