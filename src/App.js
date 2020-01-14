bd489f29c0bd7ac7609e13bed337307bbeda6dbd
devarcher <archershot@hotmail.com>
2020-01-13 20:31:27 -0600

    added comments regarding moving favorites around

--- src/App.js
+++ src/App.js
@@ -26,7 +26,8 @@ class App extends Component {
         description: beer.description,
         abv: beer.abv,
         ibu: beer.ibu,
-        image: beer.image_url
+        image: beer.image_url,
+        isLiked: false
       }));
       this.setState({ beers: filteredBeers, isLoading: false });
       // console.log('***STATE BEER***' , this.state.beers)
@@ -53,3 +54,9 @@ class App extends Component {
 }
 
 export default App;
+
+// Added isLiked with the data it will be piggy backed down the chain
+// Make onClick Handler at the top level to "Drill Down" into lower level
+// Components.  Pass onClick handler down in props to component that needs it. 
+// Pass beer.id back up to Top level and put that into a Favorites array.  
+// At this point --- [favorites] gets passed down and is mapped over and rendered.
